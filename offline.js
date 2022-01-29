8621a75c08ddc8d1af8ae86bfb59ee683f210e4c
lenghia031120 <56626129+lenghia031120@users.noreply.github.com>
2022-01-29 13:36:27 +0700

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,16 +1,11 @@
 ﻿{
-	"version": 1643342974,
+	"version": 1643438115,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
-		"images/start-sheet0.png",
-		"images/stop-sheet0.png",
-		"images/reestart-sheet0.png",
-		"images/sprite2-sheet0.png",
-		"images/next1-sheet0.png",
-		"images/next2-sheet0.png",
+		"images/sprite5-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
